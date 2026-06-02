22bc31a136a34d9a62e3e76a4766c0d480321508
Nallard92 <nicolas.allard.1@ens.etsmtl.ca>
2026-06-02 16:06:05 -0400

    feat : ajout des calcusl du temps et la réalisation des graphiques de convergence et de temps d'éxécution.

--- methode_rectangle.py
+++ methode_rectangle.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.integrate import quad
-
+import timeit
+import matplotlib.pyplot as plt
 
 
 def rectangle_python_classique(p1,p2,p3,p4,a,b,n):
@@ -36,9 +37,62 @@ def rectangle_numpy(p1,p2,p3,p4,a,b,n):
     integrale=np.sum(calculer_valeur_y(p1,p2,p3,p4,x)*taille_intervalle)
     return integrale
 
+def tracer_courbe_convergence_rectangle(p1,p2,p3,p4,a,b,valeur_n):
 
+    erreurs = []
+    for i in valeur_n:
+        erreurs.append(erreur_integration(p1,p2,p3,p4,a,b,i))
+        print(f"la valeur de n est de {i}")
+    print (f"les erreurs sont de : {erreurs}")
+
+    plt.figure()
+
+    plt.loglog(valeur_n, erreurs, 'o-')
+
+    plt.xlabel("Nombre de rectangles n")
+    plt.ylabel("Erreur absolue")
+    plt.title("Convergence de la méthode des rectangles")
+    plt.grid(True)
+
+    plt.show()
+
+
+def tracer_courbe_temps_execution(p1,p2,p3,p4,a,b,valeur_n):
+
+    temps_python_classique = []
+    temps_numpy = []
+    temps_scipy = []
+
+    for i in valeur_n:
+        temps_python_classique.append(timeit.timeit(lambda : rectangle_python_classique(1,2,3,4,0,10,100),number=10)/10)
+        temps_numpy.append(timeit.timeit(lambda: rectangle_numpy(1, 2, 3, 4, 0, 10, 100), number=10) / 10)
+        temps_scipy.append(timeit.timeit(lambda: quad(lambda x: calculer_valeur_y(p1,p2,p3,p4,x), a, b), number=10) / 10)
+
+        print(f"la valeur de n est de {i}")
+
+
+    plt.figure()
+
+    plt.loglog(valeur_n, temps_python_classique, 'o-', label='Rectangle python classique')
+    plt.loglog(valeur_n, temps_numpy, 's-', label='Rectangle NUMPY')
+    plt.loglog(valeur_n, temps_scipy, '^-', label='Scipy')
+
+    plt.xlabel("Nombre de rectangles n")
+    plt.ylabel("Temps d'exécution")
+    plt.title("Temps d'exécution de la méthode des rectangles")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
 
 
 print(f"Voici la réponse de l'intégrale : {rectangle_python_classique(1,2,3,4,0,10,10)}")
 print(f"Voici la réponse de l'intégrale : {rectangle_numpy(1,2,3,4,0,10,10)}")
 erreur_integration(1,2,3,4,0,10,10)
+
+#temps_python=timeit.timeit(lambda : rectangle_python_classique(1,2,3,4,0,10,100),number=100)
+#print(f"Temps d'integration python : {temps_python/100}")
+
+#temps_numpy=timeit.timeit(lambda : rectangle_numpy(1,2,3,4,0,10,100),number=100)
+#print(f"Temps d'integration numpy : {temps_numpy/100}")
+
+tracer_courbe_temps_execution(1,2,3,4,0,10,[10,50,100,500,1000,5000])
