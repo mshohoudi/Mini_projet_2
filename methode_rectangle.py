8e0e3d96f46223c27a6f0447ccc9d3f66ee39f56
Nallard92 <nicolas.allard.1@ens.etsmtl.ca>
2026-06-02 14:38:29 -0400

    feat : calcul intégralce rectangle en python classique

---
+++ methode_rectangle.py
@@ -0,0 +1,18 @@
+def rectangle_python_classique(p1,p2,p3,p4,a,b,n):
+    taille_intervalle=(b-a)/n
+
+
+    valeur_y = []
+    integrale =0
+    for i in range(n):
+        valeur_y.append(calculer_valeur_y(p1,p2,p3,p4,a+taille_intervalle/2+i*taille_intervalle))
+        print (f"voici la liste des valeurs en y {valeur_y}")
+        hauteur_rectangle=(calculer_valeur_y(p1, p2, p3, p4, a + taille_intervalle / 2 + i * taille_intervalle))
+        integrale+=hauteur_rectangle*taille_intervalle
+        print (f"voici l'intgrale des valeurs en y {integrale}")
+    return integrale
+
+def calculer_valeur_y(p1,p2,p3,p4,x):
+    return p1+p2*x+p3*x**2+p4*x**3
+
+print(f"Voici la réponse de l'intégrale : {rectangle_python_classique(1,2,3,4,0,10,10)}")
